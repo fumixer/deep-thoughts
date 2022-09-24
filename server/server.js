14ea712fabe08825d05953b9c477ad5a8a04232e
fumixer <fumixer@gmail.com>
2022-09-24 14:37:42 -0400

    initial

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
