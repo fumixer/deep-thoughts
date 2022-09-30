50c7790baae857b4436b3f4dcccf46bcb26f5cc6
fumixer <fumixer@gmail.com>
2022-09-29 21:53:21 -0400

    react router

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -20,6 +21,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
   
   db.once('open', () => {
     app.listen(PORT, () => {
