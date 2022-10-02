54d23625b405c008607b5c1a116f45146882409f
fumixer <fumixer@gmail.com>
2022-10-01 21:35:32 -0400

    ThoughtForm

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
-
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
@@ -31,6 +33,16 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <h3 className="bg-dark text-secondary p-3 display-inline-block">
@@ -38,7 +50,18 @@ const Profile = () => {
       </h3>
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
+
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+
+          <h2 className="bg-dark text-secondary p-3 display-inline-block">
+            Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          </h2>
+
+          {userParam && (
+            <button className="btn ml-auto" onClick={handleClick}>
+              Add Friend
+            </button>
+          )}
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
@@ -49,6 +72,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
